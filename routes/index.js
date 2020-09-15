c1ef597ecc90236a8e5ad665e235643a609df3bd
elloot <elliot.duchek@gmail.com>
2020-09-15 11:16:07 +0200

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Express',
+    test: 'BRA SIDA',
+    lorem: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum laboriosam sunt deleniti, repellendus maiores facilis eius dignissimos excepturi obcaecati nam, impedit voluptatem quod ratione voluptas unde ducimus rem mollitia eos.'
+  });
+});
+
+router.get('/boot', (req, res, next) => {
+  res.render('bootstrap');
+});
+
+module.exports = router;
